37470dde758bce223a037a80f4a5991f7d2c3870
gmkapilan <gmkapilan@gmail.com>
2025-01-03 21:17:21 -0800

    Updates to add for Zexit

--- basicpic.py
+++ basicpic.py
@@ -1,14 +1,15 @@
 
 import os
 import time
+import subprocess
 from picamera2 import Picamera2
 
 REPO_PATH = "/home/BrownianMotion/gitrepo/CubeSat_BM"     #Your github repo path: ex. /home/pi/FlatSatChallenge
-FOLDER_PATH = "/home/BrownianMotion/images/captured_image.jpg"   #Your image folder path in your GitHub repo: ex. /Images
+FOLDER_PATH = "/home/BrownianMotion/gitrepo/CubeSat_BM/captured_image.jpg"   #Your image folder path in your GitHub repo: ex. /Images
 def take_pic():
 	picam2 = Picamera2()
 	picam2.configure(picam2.create_still_configuration())
-	save_path = os.path.expanduser("/home/BrownianMotion/images/captured_image.jpg")
+	save_path = os.path.expanduser(FOLDER_PATH)
 	picam2.start()
 	time.sleep(2)
 	picam2.capture_file(save_path)
@@ -20,15 +21,10 @@ def git_push():
     This function is complete. Stages, commits, and pushes new images to your GitHub repo.
     """
     try:
-        repo = Repo(REPO_PATH)
-        origin = repo.remote('origin')
-        print('added remote')
-        origin.pull()
-        print('pulled changes')
-        repo.git.add(REPO_PATH + FOLDER_PATH)
-        repo.index.commit('New Photo')
-        print('made the commit')
-        origin.push()
+        subprocess.run(["cd", REPO_PATH], shell=True, check=True)
+        subprocess.run(["git", "add", FOLDER_PATH], check=True)
+        subprocess.run(["git", "commit", "-m", "First image commit"], check=True)
+        subprocess.run(["git", "push"], check=True)
         print('pushed changes')
     except:
         print('Couldn\'t upload to git')
